14493fd5cd9e0d3d9a9f842be0aa41031a798b7a
Jakub <44299334+gawronskijakub@users.noreply.github.com>
2023-03-12 12:52:34 +0100

    Added Hamburger

---
+++ src/components/Hamburger/Hamburger.tsx
@@ -0,0 +1,17 @@
+import { MouseEvent } from 'react';
+import styles from './Hamburger.module.scss';
+
+export const Hamburger = (): JSX.Element => {
+  const handleClick = (e: MouseEvent) => {
+    e.currentTarget.classList.toggle(styles['Open']);
+  };
+
+  return <div className={styles['Hamburger']} onClick={(e) => {handleClick(e)}}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import Hamburger from '../Hamburger';
 import styles from './Header.module.scss';
 
 export const Header = (): JSX.Element => {
 	return (
 		<header className={styles['Header']}>
 			<h1 className={styles['Header__Title']}>Arrangers</h1>
+			<Hamburger />
 		</header>
 	);
 };
