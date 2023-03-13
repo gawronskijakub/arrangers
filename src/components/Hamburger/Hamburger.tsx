410db2a187938a957b3762275d4466603ad99d6f
Jakub <44299334+gawronskijakub@users.noreply.github.com>
2023-03-13 18:58:19 +0100

    Added mobile navigation

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -1,9 +1,15 @@
 import { MouseEvent } from 'react';
+import navigationStyles from '@Components/Navigation/Navigation.module.scss';
 import styles from './Hamburger.module.scss';
 
 export const Hamburger = (): JSX.Element => {
 	const handleClick = (e: MouseEvent) => {
-		e.currentTarget.classList.toggle(styles['Open']);
+		const currentTarget = e.currentTarget;
+
+		currentTarget.classList.toggle(styles['Open']);
+		currentTarget.previousElementSibling?.classList.toggle(
+			navigationStyles['Open']
+		);
 	};
 
 	return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import Hamburger from '@Components/Hamburger';
+import Navigation from '../Navigation';
 import styles from './Header.module.scss';
 
 export const Header = (): JSX.Element => {
 	return (
 		<header className={styles['Header']}>
 			<h1 className={styles['Header__Title']}>Arrangers</h1>
+			<Navigation />
 			<Hamburger />
 		</header>
 	);
